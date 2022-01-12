033ae72d471e7664d21d7bb8fd85ab1eeea3d708
TDCIAN <tdcian71@gmail.com>
2022-01-12 22:57:04 +0900

    add CenterAPI

---
+++ FindCoronaCenter/Network/CenterNetwork.swift
@@ -0,0 +1,49 @@
+//
+//  CenterNetwork.swift
+//  FindCoronaCenter
+//
+//  Created by JeongminKim on 2022/01/12.
+//
+
+import Foundation
+import Combine
+
+class CenterNetwork {
+    private let session: URLSession
+    let api = CenterAPI()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func getCenterList() -> AnyPublisher<[Center], URLError> {
+        guard let url = api.getCenterListComponents().url else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("Infuser OVwiOgSnXv1FXAfkkB7QNtQINWlue3GH0paDvW3VtshFtupjcb6USMZASpd8L3OPo62zKNAGmsBng5KEPpxUpA==", forHTTPHeaderField: "Authorization")
+        
+        return session.dataTaskPublisher(for: request)
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw URLError(.unknown)
+                }
+                
+                switch httpResponse.statusCode {
+                case 200..<300:
+                    return data
+                case 400..<500:
+                    throw URLError(.clientCertificateRejected)
+                case 500..<599:
+                    throw URLError(.badServerResponse)
+                default:
+                    throw URLError(.unknown)
+                }
+            }
+            .decode(type: CenterAPIResponse.self, decoder: JSONDecoder())
+            .map { $0.data }
+            .mapError { $0 as! URLError }
+            .eraseToAnyPublisher()
+    }
+}
